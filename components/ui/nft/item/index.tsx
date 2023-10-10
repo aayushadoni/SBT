523784b7f1929f7dda9226060eb542a83be0bf23
aayushadoni <78133084+aayushadoni@users.noreply.github.com>
2023-10-10 13:19:08 +0530

    Update index.tsx

--- components/ui/nft/item/index.tsx
+++ components/ui/nft/item/index.tsx
@@ -1,20 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
-
 import { FunctionComponent } from "react";
-import { NftMeta,Nft } from "../../../../types/nft";
+import { NftMeta, Nft } from "../../../../types/nft";
+import Image from "next/image"; // Import the next/image component
 
 type NftItemProps = {
   item: Nft;
-}
+};
 
-const NftItem: FunctionComponent<NftItemProps> = ({item}) => {
+const NftItem: FunctionComponent<NftItemProps> = ({ item }) => {
   return (
     <>
       <div className="flex-shrink-0">
-        <img
-          className={`h-72 w-full object-cover`}
+        {/* Use the next/image component for better image optimization */}
+        <Image
+          className="h-72 w-full object-cover"
           src={item.meta.image}
           alt="New NFT"
+          width={400} // Adjust the width and height as needed
+          height={300}
         />
       </div>
       <div className="flex-1 bg-white p-6 flex flex-col justify-between">
@@ -31,23 +34,27 @@ const NftItem: FunctionComponent<NftItemProps> = ({item}) => {
           <dl className="-mx-4 -mt-4 flex flex-wrap">
             <div className="flex flex-col px-4 pt-4">
             </div>
-            { item.meta.attributes
-                .filter(Nftattribute => Nftattribute.detail_type === "Name" || Nftattribute.detail_type === "Web3_Address")
-                .map(Nftattribute =>
-              <div  className="flex flex-col px-4 pt-4">
-                <dt className="order-2 text-sm font-medium text-gray-600">
-                  {Nftattribute.detail_type}
-                </dt>
-                <dd className="order-1 text-xl font-extrabold text-indigo-600">
-                  {Nftattribute.value}
-                </dd>
-              </div>
-            )}
+            {item.meta.attributes
+              .filter(
+                (Nftattribute) =>
+                  Nftattribute.detail_type === "Name" ||
+                  Nftattribute.detail_type === "Web3_Address"
+              )
+              .map((Nftattribute) => (
+                <div className="flex flex-col px-4 pt-4" key={Nftattribute.value}>
+                  <dt className="order-2 text-sm font-medium text-gray-600">
+                    {Nftattribute.detail_type}
+                  </dt>
+                  <dd className="order-1 text-xl font-extrabold text-indigo-600">
+                    {Nftattribute.value}
+                  </dd>
+                </div>
+              ))}
           </dl>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default NftItem;
